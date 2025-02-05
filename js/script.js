af57bf2a096370b83c50708a5910aad226809143
shohag <109307621+shohag989@users.noreply.github.com>
2025-02-06 00:33:06 +0600

    beautyfied index.html file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
-    header.classList.toggle("sticky", window.scrollY > 200)
+    header
+        .classList
+        .toggle("sticky", window.scrollY > 200)
 });
 
 var typed = new Typed(".auto_input", {
-    strings: ["Web Designer", "Web Developer", "SEO Mearketer"],
+    strings: [
+        "Web Designer", "Web Developer", "SEO Mearketer"
+    ],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
